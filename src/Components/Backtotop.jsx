cd3bbd219c35c610b35a25ae1b187719c021f827
naman-398 <you@example.com>
2024-02-22 13:58:13 +0530

    update2

--- src/Components/Backtotop.jsx
+++ src/Components/Backtotop.jsx
@@ -23,7 +23,7 @@ const Backtotop = () => {
     <button className={`back-to-top ${isVisible ? 'visible' : ''} fixed bottom-[20px] right-[20px] z-[60]`}
     onClick={scrollToTop}
     style={{ display: isVisible ? 'block' : 'none',}} >
-        <div className='w-[30px] h-[30px] 450:w-[50px] 450:h-[50px] rounded-full bg-[#D45B07] flex justify-center items-center'>
+        <div className='w-[30px] h-[30px] 450:w-[50px] p-[7px] 450:p-0 450:h-[50px] rounded-full bg-[#D45B07] flex justify-center items-center'>
 <Backtop/>
 </div>
 </button>
